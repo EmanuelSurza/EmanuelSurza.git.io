723182a2e8bd639bf813f6cf256257f7d07aa4bc
EmanuelSurza <emanuelsurza@gmail.com>
2023-03-18 14:20:11 -0300

    Update Java

--- js/java.js
+++ js/java.js
@@ -6,15 +6,15 @@ const menu = document.querySelector('.menu');
 const menuLista = document.querySelector('.menu__lista');
 
 botonDemoWordleGame.onclick = function (){
-  location.assign("../WordleGameWithHtml/index.html");
+  window.location.href = "../WordleGameWithHtml/index.html";
 }
 
 botonDemoEncriptar.onclick = function (){
-  location.assign("../Encriptador/index.html"); 
+  window.location.href = "../Encriptador/index.html"; 
 }
 
 botonDemoAhorcado.onclick = function (){ 
-  location.assign("../Ahorcado/indexAhorcado.html");
+  window.location.href = "../Ahorcado/indexAhorcado.html";
 }
 
 window.addEventListener('scroll', () => {
